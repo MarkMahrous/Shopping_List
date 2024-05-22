ebad144d04c0920294906f32f9587ee09925f2bd
MarkMahrous <markmahrous012@gmail.com>
2024-05-22 20:34:21 +0300

    adding http package to shopping list so we can connect our backend to the application

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:shopping_list/data/categories.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 
@@ -18,15 +21,28 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _enteredCategory = categories[Categories.vegetables]!;
 
-  void _saveForm() {
+  void _saveForm() async {
     if (_formkey.currentState!.validate()) {
       _formkey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _enteredCategory,
-      ));
+      final url = Uri.https('flutter-prep-7b859-default-rtdb.firebaseio.com',
+          'shopping_list.json');
+      final response = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'name': _enteredName,
+          'quantity': _enteredQuantity,
+          'category': _enteredCategory.title,
+        }),
+      );
+      // Navigator.of(context).pop(GroceryItem(
+      //   id: DateTime.now().toString(),
+      //   name: _enteredName,
+      //   quantity: _enteredQuantity,
+      //   category: _enteredCategory,
+      // ));
       print(_enteredName);
       print(_enteredQuantity);
       print(_enteredCategory.title);
